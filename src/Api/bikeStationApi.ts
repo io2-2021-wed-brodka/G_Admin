a70fc6abebbb2eb347bb3eaae1a80c6e4ed29bca
Arkadiusz Górecki <53404789+arkadiusz-gorecki@users.noreply.github.com>
2021-05-06 17:57:57 +0200

    [ROW-81][ROW-95] List blocked stations and add unblock button (#20)
    * Added switch to station list page
    
    * Changed display switch
    
    * Deleted redundant console log lines
    
    * only show delete button on blocked station list if station has no bikes
    
    Co-authored-by: Tomek Karwowski <to.karwowski@gmail.com>

--- src/Api/bikeStationApi.ts
+++ src/Api/bikeStationApi.ts
@@ -1,4 +1,4 @@
-import { block_station_url, station_url } from "./urls";
+import { block_station_url, active_station_url, station_url } from "./urls";
 import {
   axiosHandleResponse,
   getRequestConfig,
@@ -34,13 +34,33 @@ export const getStations = async (): Promise<IApiResponse<Stations>> => {
     .then((r) => axiosHandleResponse(r));
 };
 
-export const blockBikeStation = async (stationID: string) => {
+export const blockStation = async (stationID: string) => {
   axios
     .post(block_station_url, { id: stationID }, getRequestConfig())
     .then((r) => axiosHandleResponse(r));
 };
 
+export const unblockStation = async (stationID: string) => {
+  const block_station_id_url = `${block_station_url}${stationID}/`;
+  axios
+    .delete(block_station_id_url, getRequestConfig())
+    .then((r) => axiosHandleResponse(r));
+};
+
+
 export const deleteBikeStation = async (stationID: string) => {
   let url = `${station_url}${stationID}/`;
   return axios.delete(url, getRequestConfig());
+};
+
+export const getActiveStations = async (): Promise<IApiResponse<Stations>> => {
+  return axios
+    .get(active_station_url, getRequestConfig())
+    .then((r) => axiosHandleResponse(r));
+};
+
+export const getBlockedStations = async (): Promise<IApiResponse<Stations>> => {
+  return axios
+    .get(block_station_url, getRequestConfig())
+    .then((r) => axiosHandleResponse(r));
 };
