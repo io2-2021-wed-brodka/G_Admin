2bb528f136b6f364ef61a923955f7a0dc9258c20
Tomek Karwowski <to.karwowski@gmail.com>
2021-06-15 09:26:58 +0200

    differentiate between dev and prod environment

--- src/Api/urls.ts
+++ src/Api/urls.ts
@@ -1,4 +1,8 @@
-const BASE_URL = 'http://ec2-18-156-197-166.eu-central-1.compute.amazonaws.com:8080/';
+const env = process.env.REACT_APP_STAGE;
+
+const prodUrl = 'http://ec2-18-156-197-166.eu-central-1.compute.amazonaws.com:8080/';
+export const BASE_URL = env === 'prod' ? prodUrl : 'http://localhost:8000';
+
 export const station_url = BASE_URL + "stations";
 export const block_station_url = station_url + "/blocked";
 export const active_station_url = station_url + "/active";
